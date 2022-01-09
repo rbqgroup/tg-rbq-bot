e64e37d44db7d789c4730a8da713cb8589da8323
神楽坂雅詩 <cyscxchope@gmail.com>
2022-01-09 19:02:59 +0800

    挣脱口塞球

--- yashibot.py
+++ yashibot.py
@@ -15,7 +15,8 @@ print('正在初始化...')
 c_TGTOKEN = '*:*'
 c_REDIS = ['127.0.0.1', 6379, '*']
 c_REDISDB = [10]  # 配置庫
-c_CHAR = [['呜', '嘤', '哈', '啊', '唔', '嗯'], ['！', '？', '…']]
+c_CHAR: list[list[str]] = [['呜', '嘤', '哈', '啊', '唔', '嗯'], ['！', '？', '…']]
+c_CHARALL: list[str] = c_CHAR[0] + c_CHAR[1]
 
 updater = Updater(token=c_TGTOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -52,6 +53,66 @@ updater.start_polling()
 
 def echo(update: Update, context: CallbackContext):
     """回顯收到的所有非命令訊息"""
+    if isPermission(update.message.chat.id) == False:
+        return
+    text: str = update.message.text
+    if len(text) == 0 or text[0] == '/':
+        return
+    fromUser: str = '@'+update.message.from_user.username
+    chatID: int = update.message.chat.id
+    redis_conn = redis.Redis(connection_pool=redis_pool)
+    rediskey: str = 'gag_' + str(chatID) + '_' + str(fromUser)
+    gagInfo = redis_conn.get(rediskey)
+    if gagInfo != None and len(gagInfo) > 0:
+        gagInfo = gagInfo.decode()
+        if gagInfo == '0':
+            redis_conn.close()
+            return
+        infoArr = json.loads(gagInfo)
+        gagTotal: int = int(infoArr[0])
+        gagTotal -= 1
+        if gagTotal <= 0:
+            redis_conn.set(rediskey, '0', ex=60)
+            names: list[str] = []
+            first = True
+            for name in infoArr:
+                if first:
+                    first = False
+                else:
+                    names.append(name)
+            alert: str = fromUser + ' 挣脱了被 ' + \
+                (' 、 '.join(names))+' 佩戴或加固的口塞球！现在可以自由说话了！（ 1分钟强制休息时间，期间不能再被佩戴口球）'
+            print(alert)
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text=alert)
+        else:
+            isOK = True
+            for msgChar in text:
+                isInChar = False
+                for dbChar in c_CHARALL:
+                    if msgChar == dbChar:
+                        isInChar = True
+                        break
+                if isInChar == False:
+                    isOK = False
+                    break
+            if isOK:
+                infoArr[0] = gagTotal
+                gagInfo = json.dumps(infoArr)
+                redis_conn.set(rediskey, gagInfo)
+                gagTotalStr: str = str(gagTotal)
+                print(fromUser+' -1 = '+gagTotalStr)
+                singleNum: str = gagTotalStr[-1]
+                if singleNum == '5' or singleNum == '0':
+                    alert = fromUser+' 加油！还有 '+gagTotalStr+' 次！'
+                    print(alert)
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, text=alert)
+            else:
+                context.bot.delete_message(
+                    chat_id=update.message.chat_id, message_id=update.message.message_id)
+                print(fromUser+' -0 = '+str(gagTotal))
+    redis_conn.close()
 
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
@@ -68,13 +129,14 @@ def gag(update: Update, context: CallbackContext):
     fromUser: str = '@'+update.message.from_user.username
     chatID: int = update.message.chat.id
     redis_conn = redis.Redis(connection_pool=redis_pool)
-    rediskey: str = 'gag_' + chatID + '_' + str(toUser)
-    gagInfo: str = redis_conn.get(rediskey).decode()
+    rediskey: str = 'gag_' + str(chatID) + '_' + str(toUser)
+    gagInfo = redis_conn.get(rediskey)
     alert: str = ''
     if gagInfo != None and len(gagInfo) > 0:
+        gagInfo = gagInfo.decode()
         if gagInfo == '0':
             redis_conn.close()
-            alert = toUser+'刚刚挣脱口塞球！请给对方 1 分钟的休息时间！'
+            alert = toUser+' 刚刚挣脱口塞球！请给对方 1 分钟的休息时间！'
             print(alert)
             context.bot.send_message(
                 chat_id=update.effective_chat.id, text=alert)
@@ -98,7 +160,6 @@ def gag(update: Update, context: CallbackContext):
             infoArr.append(fromUser)
             gagInfo = json.dumps(infoArr)
             redis_conn.set(rediskey, gagInfo)
-            redis_conn.close()
             alert = fromUser+' 加固了 '+toUser+' 的口塞球！'
         alert += ' '+toUser+' 目前佩戴着被 ' + \
             (' 、 '.join(names))+' 安装或加固的口塞球，还需要挣扎 '+str(gagTotal)+' 次才能把它挣脱！'
@@ -106,11 +167,11 @@ def gag(update: Update, context: CallbackContext):
         infoArr = [5, fromUser]
         gagInfo = json.dumps(infoArr)
         redis_conn.set(rediskey, gagInfo)
-        redis_conn.close()
         alert = fromUser+' 为 '+toUser+' 戴上了口塞球！ '+toUser+' 必须挣扎 5 次才能挣脱它！其他人可以继续用同样指令加固 '+toUser+' 的口塞球（但同一个人只能在对方挣脱后才能再次为对方佩戴或加固口塞球）。 ' + \
             toUser+' 现在只能在消息中发送以下文字「' + \
             ('、'.join(c_CHAR[0]))+'」和中文标点「' + \
             ('、'.join(c_CHAR[1]))+'」，每发送一条消息算作挣扎一次，包含其他字符的消息不能发送！'
+    redis_conn.close()
     if fromUser == toUser:
         alert += '居然自己给自己戴口塞球，真是个可爱的绒布球呢！'
     print(alert)
